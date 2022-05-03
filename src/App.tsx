5fd31bb58571165364c2cfef088dbfa006177853
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2022-05-03 22:54:38 +0300

    some fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,9 @@ import { Provider } from 'react-redux'
 import { Map } from './components/Map/Map'
 import { Menu } from './components/Menu/Menu'
 import { Burger } from './components/Burger/Burger'
-import useVH from 'react-vh'
 
 export const App = () => {
   
-  useVH()
-
   const store = setupStore()
 
   return (
